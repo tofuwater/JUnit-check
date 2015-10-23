21a23c615a6935aac5e9df922d151bd9b833c75f
“james <james.colley@nottingham.ac.uk>
2015-10-23 14:25:15 +0100

    reverted back to broken version

--- src/PennyLoan.java
+++ src/PennyLoan.java
@@ -3,7 +3,8 @@ public class PennyLoan
         private int amount ; // Amount of loan in pence
         private int length ; // length of subscription in months
         
-        public PennyLoan(int a, int l) {
+        public PennyLoan(int a, int l) 
+        {
                 amount = a ;
                 length = l ;
         }
@@ -12,9 +13,10 @@ public class PennyLoan
         * Calculate the monthly payment in pounds,
         * rounded up to the nearest penny.
         */
-        
-        public double paymentPerMonth() {
-                double r = Math.ceil((((double) amount/100)/ (double) length)*100)/100;
+        public double paymentPerMonth() 
+        {
+                double r = (double) amount/ (double) length;
                 return r ;
         }
 }
+
